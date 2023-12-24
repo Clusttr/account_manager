5bad48d78da4c70cbbed068f3f176a4b3925e5a2
Chukwuemeka Matthew <matthew.chukwuemeka40@gmail.com>
2023-12-24 21:02:01 +0100

    run program in app/main

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,92 @@
-// import * as anchor from "@coral-xyz/anchor";
-// import {Program} from "@coral-xyz/anchor";
-// import { AccountManager } from "../target/types/account_manager";
-//
-// const provider = anchor.AnchorProvider.env()
-// anchor.setProvider(provider);
-// const payer = provider.wallet as anchor.Wallet
-// const program = anchor.workspace.AccountManager as Program<AccountManager>;
+import {AnchorProvider, Idl, Program, Wallet, web3, BN} from "@coral-xyz/anchor";
+import * as fs from "fs";
+import idl from "../target/idl/account_manager.json";
+import { AccountManager, IDL } from "../target/types/account_manager"
+import {Keypair, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+
+let solanaURL = "https://api.devnet.solana.com" // "http://localhost:8899"
+function loadWalletKey(): web3.Keypair {
+     return web3.Keypair.fromSecretKey(
+         new Uint8Array(JSON.parse(fs.readFileSync("/Users/matthewchukwuemeka/.config/solana/id.json").toString()))
+         // new Uint8Array()
+     )
+}
+const connection = new web3.Connection(solanaURL)
+const walletKeypair = loadWalletKey()
+const wallet = new Wallet(walletKeypair)
+
+const programId = new web3.PublicKey(idl.metadata.address)
+let [vaultAddress] = PublicKey.findProgramAddressSync(
+    [Buffer.from("rent_vault")],
+    programId
+)
 
 async function main() {
-    // console.log("Payer: ", payer.publicKey)
-    console.log("Hello world")
+    const provider = new AnchorProvider(connection, wallet, {})
+
+    const program = new Program<AccountManager>(IDL, programId, provider)
+
+    const vaultAccountInfo = await connection.getAccountInfo(vaultAddress)
+    console.log("Vault Account: ", vaultAccountInfo)
+
+    // await fundVault(1, vaultAddress, program)
+    // await withdrawFromVault(0.5, program)
+
+    const newAccount = Keypair.generate()
+    console.log({publicKey: newAccount.publicKey.toBase58(), se: newAccount.secretKey})
+    // await registerNewAccount(newAccount, program)
+
+    const vaultAccountInfo2 = await connection.getAccountInfo(vaultAddress)
+    console.log("Vault Account2: ", vaultAccountInfo2)
+
 }
 
-main().then(() => {
-    console.log("End of program")
-}).catch(err => {
-    console.log("Error: ", err)
-})
+async function fundVault(amount: number, vault: PublicKey, program: Program<AccountManager>) {
+    let fundAmount = new BN(LAMPORTS_PER_SOL * amount)
+    const tx = await program.methods.fundRentVault(fundAmount)
+        .accounts({
+            payer: wallet.payer.publicKey,
+            rentVault: vault
+        })
+        .rpc()
+
+    console.log(`******${tx}*******`)
+}
+
+async function withdrawFromVault(amount: number, program: Program<AccountManager>) {
+    let fundToWithdraw = new BN(LAMPORTS_PER_SOL * amount)
+    const tx = await program.methods
+        .withdrawRentVault(fundToWithdraw)
+        .accounts({
+            payer: wallet.payer.publicKey,
+            rentVault: vaultAddress
+        })
+        .rpc()
+    console.log(`***WithdrawFromVault***${tx}*******`)
+}
+
+async function registerNewAccount(account: Keypair, program: Program<AccountManager>) {
+    const tx = await program.methods
+        .registerNewAccount()
+        .accounts({
+            newAccount: account.publicKey,
+            rentVault: vaultAddress
+        })
+        .signers([account])
+        .rpc()
+
+    console.log(`***RegisterNewAccount***${tx}*******`)
+}
+
+main()
+  .then(() => {
+      printLine("END OF PROGRAM")
+  })
+  .catch((err) => {
+      printLine("END OF PROGRAM WITH ERROR")
+      console.log("Error: ", err);
+  });
+
+function printLine(msg: string) {
+    console.log(`-------------------------${msg}-------------------------`)
+}
--- tests/account-manager.ts
+++ tests/account-manager.ts
@@ -1,79 +1,80 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AccountManager } from "../target/types/account_manager";
-import {Keypair, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
-import {assert} from "chai";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("account-manager", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  const payer = provider.wallet as anchor.Wallet
+  const payer = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.AccountManager as Program<AccountManager>;
 
-    const [vaultAccount] = PublicKey.findProgramAddressSync(
-        [Buffer.from("rent_vault")],
-        program.programId
-    )
-
+  const [vaultAccount] = PublicKey.findProgramAddressSync(
+    [Buffer.from("rent_vault")],
+    program.programId
+  );
 
   it("fund rent vault", async () => {
-
-    const fundAmount = new anchor.BN(LAMPORTS_PER_SOL)
-      await program.methods
-        .fundRentVault(fundAmount)
-        .accounts({
-          payer: payer.publicKey,
-          rentVault: vaultAccount
-        })
-        .signers([payer.payer])
-        .rpc();
+    const fundAmount = new anchor.BN(LAMPORTS_PER_SOL);
+    await program.methods
+      .fundRentVault(fundAmount)
+      .accounts({
+        payer: payer.publicKey,
+        rentVault: vaultAccount,
+      })
+      .signers([payer.payer])
+      .rpc();
 
     //Check rent vault
-      const accountInfo = await program.provider.connection.getAccountInfo(vaultAccount)
-      console.log("Vault Account Info:", accountInfo)
+    const accountInfo = await program.provider.connection.getAccountInfo(
+      vaultAccount
+    );
+    console.log("Vault Account Info:", accountInfo);
   });
 
   it("register a new account using rent vault", async () => {
-      const newAccount = new Keypair()
+    const newAccount = new Keypair();
 
-      await program.methods
-          .registerNewAccount()
-          .accounts({
-              rentVault: vaultAccount,
-              newAccount: newAccount.publicKey
-          })
-          .signers([newAccount])
-          .rpc()
+    await program.methods
+      .registerNewAccount()
+      .accounts({
+        rentVault: vaultAccount,
+        newAccount: newAccount.publicKey,
+      })
+      .signers([newAccount])
+      .rpc();
 
-      const connection = program.provider.connection
-      const minRentFreeLamports = await connection.getMinimumBalanceForRentExemption(0)
-      const accountInfo = await connection.getAccountInfo(newAccount.publicKey)
+    const connection = program.provider.connection;
+    const minRentFreeLamports =
+      await connection.getMinimumBalanceForRentExemption(0);
+    const accountInfo = await connection.getAccountInfo(newAccount.publicKey);
 
-      assert(minRentFreeLamports == accountInfo.lamports)
-  })
+    assert(minRentFreeLamports == accountInfo.lamports);
+  });
 
-    it("withdraw from rent vault", async () => {
-        const withdrawAmount = new anchor.BN(LAMPORTS_PER_SOL)
-        const connection = program.provider.connection
+  it("withdraw from rent vault", async () => {
+    const withdrawAmount = new anchor.BN(LAMPORTS_PER_SOL);
+    const connection = program.provider.connection;
 
-        const vaultInfo1 = await connection.getAccountInfo(vaultAccount)
+    const vaultInfo1 = await connection.getAccountInfo(vaultAccount);
 
-        await program.methods
-            .withdrawRentVault(withdrawAmount)
-            .accounts({
-                payer: payer.publicKey,
-                rentVault: vaultAccount
-            })
-            .signers([payer.payer])
-            .rpc()
+    await program.methods
+      .withdrawRentVault(withdrawAmount)
+      .accounts({
+        payer: payer.publicKey,
+        rentVault: vaultAccount,
+      })
+      .signers([payer.payer])
+      .rpc();
 
-        const vaultInfo2 = await connection.getAccountInfo(vaultAccount)
+    const vaultInfo2 = await connection.getAccountInfo(vaultAccount);
 
-        console.log("vaultInfo1: ", vaultInfo1.lamports)
-        console.log("vaultInfo2: ", vaultInfo2.lamports)
+    console.log("vaultInfo1: ", vaultInfo1.lamports);
+    console.log("vaultInfo2: ", vaultInfo2.lamports);
 
-        console.assert(vaultInfo1.lamports > vaultInfo2.lamports)
-    })
+    console.assert(vaultInfo1.lamports > vaultInfo2.lamports);
+  });
 });
